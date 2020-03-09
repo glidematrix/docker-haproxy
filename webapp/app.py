9898f9b4aca528366fe672bbf3666aec6b9e9233
glidematrix <mundiamwala@gmail.com>
2020-03-09 17:32:11 +0300

    first commit

---
+++ webapp/app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask,jsonify
+from pprint import pprint
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+	appName = os.getenv("appName")
+
+	pprint(dir(os.environ))
+
+	res = {
+		"appName": appName
+	}
+
+	return jsonify(res)
+
+
+if __name__ == '__main__':
+	app.run(
+			host="0.0.0.0",
+			port=5000,
+			debug=True
+		)
